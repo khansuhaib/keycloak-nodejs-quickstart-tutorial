3a50e3892eb75c8d65e24241a12a10e245f4c4f1
khansuhaib <khansuhaib@gmail.com>
2021-08-25 13:49:46 -0400

    Update app.js

--- www/app.js
+++ www/app.js
@@ -1,13 +1,13 @@
 let keycloakConfig = {
     "realm": "myrealm",
-    "auth-server-url": "http://127.0.0.1:8080/auth",
+    "auth-server-url": "https://keycloak-keycloak2.apps-crc.testing/auth/",
     "ssl-required": "external",
     "resource": "web-app1",
     "credentials": {
         "secret": "90d014fa-89bf-45b5-ab87-bcd8a9028c7f"
     },
     "confidential-port": 0,
-    "enable-cors": true,
+    "enable-cors": false,
     "clientId": "web-app1"
 };
 window.keycloak = new Keycloak(keycloakConfig);
